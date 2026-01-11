337c69502a4c4c83e3005c1e6d60df480e084df8
lakshmansinghmalviya <lslakshman2024@gmail.com>
2026-01-11 22:05:06 +0530

    done with the dynamic read of the cors

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,26 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     // app.enableCors();
+    const corsOrigins = process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',').map(origin => origin.trim()) : [];
 
     app.enableCors({
-    origin: [
-      'http://localhost:5173',  // Vite
-      'http://localhost:3000',  // React
-      'https://vet-chat-bot-integrate-anywhere.vercel.app/'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  });
+        origin: corsOrigins,
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    });
+
+    // app.enableCors({
+    //     origin: [
+    //         'http://localhost:5173',  // Vite
+    //         'http://localhost:3000',  // React
+    //         'https://vet-chat-bot-integrate-anywhere.vercel.app/',
+    //         'https://vet-demo-six.vercel.app/'
+    //     ],
+    //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    //     allowedHeaders: ['Content-Type', 'Authorization'],
+    //     credentials: true
+    // });
 
     app.setGlobalPrefix('api');
 
