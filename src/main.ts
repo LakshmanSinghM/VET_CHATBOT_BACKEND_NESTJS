0ba6508d0cb6c4d256171e028089ddabce0d578a
lakshmansinghmalviya <lslakshman2024@gmail.com>
2026-01-11 21:06:07 +0530

    Done with the fix cors enabled

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,23 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.enableCors();
+    // app.enableCors();
+
+    app.enableCors({
+    origin: [
+      'http://localhost:5173',  // Vite
+      'http://localhost:3000',  // React
+      'https://vet-chat-bot-integrate-anywhere.vercel.app/'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  });
+
     app.setGlobalPrefix('api');
 
     const port = process.env.PORT || 5000;
     await app.listen(port);
-    console.log(`Application is running on: http://localhost:${port}`);
+    console.log(`Application is running on: ${port}`);
 }
 bootstrap();
